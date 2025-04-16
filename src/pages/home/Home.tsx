00fa482c391d99285e8b217e678eee9f9189f14c
Daniele Napoleao <daniele20@gmail.com>
2025-04-16 12:36:31 -0300

    Componentes Home, NavBar e Footer

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,24 +6,24 @@ function Home() {
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
+                            Seja Bem Vindo!
                         </h2>
                         <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
+                            Tudo o que você precisa, aqui!
                         </p>
 
                         <div className="flex justify-around gap-4">
                             <div className='rounded text-white 
                                             border-white border-solid border-2 py-2 px-4'
                                 >
-                                Nova Postagem
+                                Nova Categoria
                             </div>
                         </div>
                     </div>
 
                     <div className="flex justify-center ">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
+                            src="https://ik.imagekit.io/2zvbvzaqt/Acne%20treatment-amico.png?updatedAt=1744812483586"
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
